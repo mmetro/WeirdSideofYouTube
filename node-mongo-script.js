e55b3d19943e8f84b7df55b2d33188bc6e6b639b
mmetro <m.metrocavich@gmail.com>
2015-12-01 11:39:07 -0500

    Change random range to size of collection

--- node-mongo-script.js
+++ node-mongo-script.js
@@ -1,5 +1,3 @@
-  GNU nano 2.2.6                                                                   File: test.js                                                                                                                                   Modified  
-
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
@@ -9,12 +7,12 @@ var url = 'mongodb://localhost:27017/test';
 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
-        db.collection('youtubes').findOne({_id: Math.floor(Math.random() * 196) + 1}, function(err, document) {
-                console.log(document);
-                db.close();
-        });
+ db.collection('videos', function(err, coll) {
+     coll.count(function(err, result){
+      db.collection('youtubes').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
+        console.log(document);
+        db.close();
+      });
+    });
+  });
 });
-
-
-
-
