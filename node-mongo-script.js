7b2a7d9eb13cfca424690a57023dc7dc17c82075
mmetro <m.metrocavich@gmail.com>
2015-12-04 16:41:58 -0500

    Serve up index.html; Require express

--- node-mongo-script.js
+++ node-mongo-script.js
@@ -3,16 +3,28 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
 
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
+app.use(express.static(__dirname + '/'));
 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
- db.collection('videos', function(err, coll) {
-     coll.count(function(err, result){
-      db.collection('youtubes').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
-        console.log(document);
-        db.close();
+app.get('/',function(req,res){
+
+  res.sendFile(path.join(__dirname+'/index.html'));
+
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('videos', function(err, coll) {
+       coll.count(function(err, result){
+        db.collection('youtubes').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
+          console.log(document);
+          console.log(document["Video UR"]);
+          db.close();
+        });
       });
     });
   });
 });
+
+app.listen(3000);
