db7f5ef063b4df15b4c4491cbbe6d95dc191e758
mmetro <m.metrocavich@gmail.com>
2015-12-04 21:47:57 -0500

    Create backend autoplay API

--- node-mongo-script.js
+++ node-mongo-script.js
@@ -17,12 +17,30 @@ app.get('/',function(req,res){
     assert.equal(null, err);
     db.collection('videos', function(err, coll) {
        coll.count(function(err, result){
-        db.collection('youtubes').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
+        db.collection('videos').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
+          console.log(result);
           console.log(document);
           randomID = document["Video UR"];
           console.log(randomID);
           db.close();
           res.render(path.join(__dirname+'/index'), { videoID: randomID});
+        });
+      });
+    });
+  });
+});
+
+app.get('/api/getvid',function(req,res){
+  var randomID;
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('videos', function(err, coll) {
+       coll.count(function(err, result){
+        db.collection('videos').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
+          randomID = document["Video UR"];
+          console.log(randomID);
+          db.close();
+          res.json({id: randomID});
         });
       });
     });
