76a113673df9191f36d1b79de275e998b439f8c2
Michael Metrocavich <mmetro@users.noreply.github.com>
2016-03-04 02:20:48 -0500

    Add code for retrieving bulk videos
    Add code for retrieving bulk videos.  Will mainly be used for the admin
    panel

--- app.js
+++ app.js
@@ -15,32 +15,45 @@ app.get('/',function(req,res){
   var randomID;
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
-    db.collection('videos', function(err, coll) {
-       coll.count(function(err, result){
-        db.collection('videos').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
-          console.log(result);
-          console.log(document);
-          randomID = document["Video UR"];
-          console.log(randomID);
-          db.close();
-          res.render(path.join(__dirname+'/index'), { videoID: randomID});
-        });
+    db.collection('videos').count(function(err, result){
+      db.collection('videos').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, { "vidID": 1, _id:0 }, function(err, document) {
+        db.close();
+        res.render(path.join(__dirname+'/index'), { videoID: document["vidID"]});
       });
     });
   });
 });
 
-app.get('/api/getvid',function(req,res){
+app.get('/api/randomvid',function(req,res){
   var randomID;
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
+    db.collection('videos').count(function(err, result){
+      db.collection('videos').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, { "vidID": 1, _id:0 }, function(err, document) {
+        db.close();
+        res.json(document);
+      });
+    });
+  });
+});
+
+
+// get up to 50 videos at once
+app.get('/api/getvidsfrom/:start/:end', function(req, res, next) {
+  var start_id = parseInt(req.params.start);
+  var end_id = parseInt(req.params.end);
+  var len = end_id - start_id + 1;
+  if(len > 50)
+  {
+    len = 50;
+  }
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
     db.collection('videos', function(err, coll) {
-       coll.count(function(err, result){
-        db.collection('videos').findOne({_id: Math.floor(Math.random() * (result-1)) + 1}, function(err, document) {
-          randomID = document["Video UR"];
-          console.log(randomID);
+      coll.count(function(err, result){
+        db.collection('videos').find({_id: {$gte: start_id }}, { "vidID": 1, _id:0 }).limit(len).toArray(  function(err, document) {
           db.close();
-          res.json({id: randomID});
+          res.json(document);
         });
       });
     });
