cd3bdde7933ad85bad93232198f12ee966e152e8
Michael Metrocavich <mmetro@users.noreply.github.com>
2016-03-29 14:39:48 -0400

    Add passport and mongoose

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ var express = require('express');
 var routes = require('./routes');
 var path = require('path');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 // global config
 var app = express();
 app.set('port', process.env.PORT || 1337);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+app.use(express.logger());
 app.use(express.favicon());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('CHANGEMESECRET'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,19 +34,21 @@ app.configure('production', function(){
     app.use(express.errorHandler());
 });
 
-// mongo config
-var MONGOLAB_URI= "add_your_mongolab_uri_here"
-var mongo = process.env.MONGOLAB_URI || 'mongodb://localhost/node-bootstrap3-template'
-mongoose.connect(mongo);
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose');
+
+// routes
+require('./routes')(app);
 
 // mongo model
 // var Model_Name = require('add_your_models_here');
 
-// routes
-app.get('/', routes.index);
-app.get('/ping', routes.ping);
-
-// run server
 app.listen(app.get('port'), function(){
-  console.log('\nExpress server listening on port ' + app.get('port'));
+  console.log(("Express server listening on port " + app.get('port')))
 });
--- models/account.js
+++ models/account.js
@@ -2,11 +2,14 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var Account = new Schema({
   username: String,
   email: String,
   time: {type: Date, default: Date.now}
 });
+
+Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Account', Account);
