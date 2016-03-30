4a79b88691a5cab76f2ad31deee2ef399dbae88a
Michael Metrocavich <mmetro@users.noreply.github.com>
2016-03-29 21:45:35 -0400

    port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var errorHandler = require('errorhandler');
 
 // global config
 var app = express();
-app.set('port', process.env.PORT || 1337);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 //app.set('view engine', 'jade');
 app.set('view engine', 'ejs');  
