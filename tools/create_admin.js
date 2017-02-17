3708335492d3466c48f69647847e83096ce3a83e
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-02-17 16:07:42 -0500

    Create tool to add administrators

---
+++ tools/create_admin.js
@@ -0,0 +1,32 @@
+#!/bin/env node
+var readline = require("readline");
+var mongoose = require("mongoose");
+var Account = require("../models/account");
+
+var database = require("../config/db");
+mongoose.connect(database.url);
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Admin username: ", (username) => {
+    rl.question("Password: ", (password) => {
+        // Create database entry
+        Account.register(new Account({ username : username, admin: true }), password, (err, account) => {
+            if (err) {
+                console.error("Could not create user in database!");
+            } else {
+                console.info("User created successfully!");
+            }
+
+            // Close stdin
+            rl.close();
+            process.stdin.destroy();
+
+            // Exit without error
+            process.exit(0);
+        });
+    });
+});
