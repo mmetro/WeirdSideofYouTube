fe45e2938d7241be06af52ef4ceb0868bbbf86c9
Jacob-Lane <jacob.lane.p@gmail.com>
2017-03-17 14:02:51 -0400

    Further improved video crawl targets

--- controllers/reddit.js
+++ controllers/reddit.js
@@ -33,7 +33,7 @@ exports.crawlRedditUrl = function(reddit_url)
 // handles the POST request for crawling reddit
 // adds the top videos to the database
 exports.crawlReddit = function() {
-  exports.crawlRedditUrl('https://www.reddit.com/r/deepintoyoutube/top/.json?limit=250&t=all'); //Get all time top videos
+  exports.crawlRedditUrl('https://www.reddit.com/r/deepintoyoutube/top/.json?limit=150&t=all'); //Get all time top videos - our bread and butter.
   setTimeout(function() {
     console.log('Finished crawling r/deepintoyoutube all time');
   }, 3000); //Short pause so we don't break reddit
@@ -41,17 +41,29 @@ exports.crawlReddit = function() {
   setTimeout(function() {
     console.log('Finished crawling r/deepintoyoutube weekly');
   }, 3000); //Short pause so we don't break reddit
-  exports.crawlRedditUrl('https://www.reddit.com/r/NotTimAndEric/top/.json?limit=50&t=all');  //Get all time top videos, only a few since it's a niche area
+  exports.crawlRedditUrl('https://www.reddit.com/r/NotTimAndEric/top/.json?limit=25&t=all');  //Get all time top videos, only a few since it's a niche area
   setTimeout(function() {
     console.log('Finished crawling r/NotTimAndEric all time');
   }, 3000); //Short pause so we don't break reddit
-  exports.crawlRedditUrl('https://www.reddit.com/r/NotTimAndEric/top/.json?limit=10&t=month'); //Get new-ish videos
+  exports.crawlRedditUrl('https://www.reddit.com/r/NotTimAndEric/top/.json?limit=5&t=month'); //Get new-ish videos
   setTimeout(function() {
     console.log('Finished crawling r/NotTimAndEric monthly');
   }, 3000); //Short pause so we don't break reddit
-  exports.crawlRedditUrl('https://www.reddit.com/r/fifthworldvideos/top/.json?limit=50&t=all'); //A bit more obscure stuff but it fits well
+  exports.crawlRedditUrl('https://www.reddit.com/r/fifthworldvideos/top/.json?limit=100&t=all'); //A bit more weird stuff but it fits well
   setTimeout(function() {
     console.log('Finished crawling r/fifthworldvideos all time');
+  }, 3000); //Short pause so we don't break reddit
+  exports.crawlRedditUrl('https://www.reddit.com/r/weirdtube/top/.json?limit=25&t=all');  //Smaller sub so only grab a few
+  setTimeout(function() {
+    console.log('Finished crawling r/weirdtube all time');
+  }, 3000); //Short pause so we don't break reddit
+  exports.crawlRedditUrl('https://www.reddit.com/r/darksideofyoutube/top/.json?limit=25&t=all');  //Smaller sub so only grab a few
+  setTimeout(function() {
+    console.log('Finished crawling r/weirdtube all time');
+  }, 3000); //Short pause so we don't break reddit
+  exports.crawlRedditUrl('https://www.reddit.com/r/unknownvideos/search.json?limit=40&q=flair%3A%22funny%22&sort=top&restrict_sr=on&t=all'); //Filter this one by flair
+  setTimeout(function() {
+    console.log('Finished crawling r/unknownvideos all time');
   }, 3000); //Short pause so we don't break reddit
   exports.crawlRedditUrl('https://www.reddit.com/r/InterdimensionalCable/top/.json?limit=50&t=all');
   setTimeout(function() {
