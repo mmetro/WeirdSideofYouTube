9290bb2ac1751a6a61a2460782f2749f7ed80278
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-03-21 19:44:37 -0400

    Get bundle working right

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // env config
 var env = process.env.NODE_ENV || 'development';
--- frontend/videos.js
+++ frontend/videos.js
@@ -1,15 +1,24 @@
 var YouTubePlayer = require('youtube-player');
 
-console.log('request');
+var player = YouTubePlayer('youtube-player', {
+  autoPlay: true,
+  preferredQuality: 'default',
+  allowFullScreen: 'true',
+  videoId: 'ENBWOIRO2WQ'
+});
 
-var player = YouTubePlayer('youtube-player-container');
+player.on('ready', function(_event) {
+  player.playVideo();
+});
 
 function playNextVid() {
-  fetch('/api/getrandomvid').then(function(vid) {
-    console.log(vid);
-    // jQuery('#youtube-player-container').tubeplayer('play', vid.vidID);
+  fetch('./api/getrandomvid').then(function(response) {
+    return response.json();
+  }).then(function(vid) {
     player.loadVideoById(vid.vidID);
     player.playVideo();
+  }).catch(function(error) {
+    console.error(error);
   });
 }
 
