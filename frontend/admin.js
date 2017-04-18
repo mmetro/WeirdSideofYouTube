8ab31b3d1b8763b1237506dc301e1cddb10d3ee7
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-04-18 14:50:36 -0400

    Build table with vanilla js

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,3 +1,6 @@
+var escape = require('escape-html');
+var ready = require('./ready');
+
 var currentVideoID = 1;
 var numVideos = 0;
 var videosPerPage = 50;
@@ -16,38 +19,71 @@ function loadNewTable() {
     }
   }
 
-  fetch('/admin/getvidrange/' + currentVideoID + '/' + (currentVideoID + 49)).then(function(response) {
+  fetch('/admin/getvidrange/' + currentVideoID + '/' + (currentVideoID + 49), {credentials: 'same-origin'}).then(function(response) {
+    console.log(response);
     return response.json();
-  }).then(function(_vids) {
-    // for(var key in vids) {
-      // var val = vids[key];
+  }).then(function(vids) {
+    for(var key in vids) {
+      var val = vids[key];
 
-
-    // }
+      var dom = generateNewEntry(val);
+      document.getElementById('videoTable').appendChild(dom);
+    }
   }).catch(function(error) {
     console.error(error);
   });
 }
 
+ready(loadNewTable);
+
 function generateNewEntry(video) {
+  // Create table row
   var tr = document.createElement('tr');
-  tr.setAttribute('id', video.videoID);
+  tr.setAttribute('id', 'vid' + escape(video.videoID));
   tr.setAttribute('class', 'videoRow');
 
   var td = document.createElement('td');
   tr.appendChild(td);
 
+  // Create removal form
   var form = document.createElement('form');
-  td.append(form);
+  td.appendChild(form);
 
   var vidIdInput = document.createElement('input');
   vidIdInput.setAttribute('type', 'hidden');
   vidIdInput.setAttribute('name', 'videoID');
-  vidIdInput.setAttribute('value', video.videoID);
+  vidIdInput.setAttribute('value', escape(video.videoID));
+  form.appendChild(vidIdInput);
 
   var submit = document.createElement('input');
   submit.setAttribute('type', 'submit');
   submit.setAttribute('value', 'Delete Video');
+  form.appendChild(submit);
+
+  // Create link to YouTube video
+  var linkTd = document.createElement('td');
+  tr.appendChild(linkTd);
+
+  var youtubeLink = document.createElement('a');
+  youtubeLink.setAttribute('href', 'https://www.youtube.com/watch?v=' + encodeURIComponent(video.videoID));
+  youtubeLink.innerText = escape(video.videoID);
+  linkTd.appendChild(youtubeLink);
+
+  // Insert other metadata
+  tr.insertAdjacentHTML('beforeEnd', '<td><img src="//i.ytimg.com/vi/' + encodeURIComponent(video.videoID) + '/default.jpg" /></td>');
+  tr.insertAdjacentHTML('beforeEnd', '<td id="title"></td>');
+  tr.insertAdjacentHTML('beforeEnd', '<td>' + escape(video.views) + '</td>');
+  tr.insertAdjacentHTML('beforeEnd', '<td>' + escape(video.skips/video.views * 100) + '%</td>');
+  tr.insertAdjacentHTML('beforeEnd', '<td>' + escape(video.errorCount/video.views * 100) + '%</td>');
+
+  // Retrieve title from server
+  fetch('/api/getVidInfo/' + encodeURIComponent(video.videoID)).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    document.querySelector('#videoTable #vid' + escape(video.videoID) + ' #title').innerText = data.items[0].snippet.title;
+  });
+
+  return tr;
 }
 
 // function loadNewTable(){
